cb93aba073a340fcdd165c950c318c9b317d4adf
Nicholas Flynt <thegamefreak0134@gmail.com>
2021-06-26 15:10:13 -0400

    Break the generator functions all out into their own file
    I might further subdivide this if I add a bunch of generators, but
    this is already much better than "stuff it all in main"

---
+++ src/mmc3_irq_table/prg/generators.s
@@ -0,0 +1,393 @@
+        .setcpu "6502"
+
+        .include "nes.inc"
+        .include "irq.inc"
+        .include "word_util.inc"
+
+        .segment "RAM"
+temp_x: .word $0000
+temp_y: .byte $00
+temp_offset: .byte $00
+irq_generation_index: .byte $00
+pixels_to_generate: .byte $00
+pixels_generated: .byte $00
+initial_pixel_offset: .byte $00
+
+.export irq_generation_index, pixels_to_generate, initial_pixel_offset
+
+        .zeropage
+fx_pattern_table_ptr: .word $0000
+fx_scanline_table_ptr: .word $0000
+fx_table_size: .byte $00
+
+.exportzp fx_pattern_table_ptr, fx_scanline_table_ptr, fx_table_size
+
+base_nametable: .byte $00
+base_x: .byte $00
+base_y: .byte $00
+
+.exportzp base_x, base_y, base_nametable
+
+        .segment "PRGLAST_E000"
+
+; for generating the funky 4th byte
+.macro nametable_low scroll_x, scroll_y
+        .byte ((scroll_x & %11111000) >> 3) | ((scroll_y & %00111000) << 2)
+.endmacro
+
+; for runtime generation without expensive bitfield manipulation, just read
+; from both tables and OR together
+nametable_low_x_table:
+        .repeat 256, scroll_x
+        .byte ((scroll_x & %11111000) >> 3)
+        .endrep
+
+nametable_low_y_table:
+        .repeat 256, scroll_y
+        .byte ((scroll_y & %00111000) << 2)
+        .endrep
+
+; =========== Various generators, mostly separated by intended function ============= ;
+
+.proc generate_x_distortion
+        lda base_nametable
+        sta temp_x+1
+        lda base_x
+        sta temp_x
+        lda base_y
+        sta temp_y
+
+        lda #$0
+        sta pixels_generated
+        ldy #$0
+
+        ; to apply the pixel offset, skip over any initial entries that are
+        ; smaller than the offset
+        lda initial_pixel_offset
+pixel_offset_loop:
+        sec
+        sbc (fx_scanline_table_ptr), y
+        bcc done_skipping_entries
+        sta initial_pixel_offset
+        iny
+        cpy fx_table_size
+        bne no_wraparound
+        ldy #$0
+no_wraparound:
+        jmp pixel_offset_loop
+done_skipping_entries:
+        ; initial_pixel_offset has been reduced to the remainder, the number of pixels
+        ; we should skip in the *current* entry, which y points to
+
+        ; for each entry in the table (16 entries):
+        ; - compute the new scroll coordinates and nametable bit
+        ; - use these values to generate one entry in the IRQ table
+        ; note: later we'll want to be able to specify the number of entries to generate,
+        ; and wrap the table around its end with a mask
+
+loop:
+        ; reset the temp X coordinate
+        lda base_nametable
+        sta temp_x+1
+        lda base_x
+        sta temp_x
+
+        ; calculate the new x offset
+        lda (fx_pattern_table_ptr), y
+        sta temp_offset
+        sadd16 temp_x, temp_offset
+
+        ; generate a new entry in the table
+        ldx irq_generation_index
+
+        ; mask the low bit of the nametable, and shift it into position
+        lda temp_x+1
+        and #$01
+        asl
+        asl
+        sta irq_table_nametable_high, x
+
+        ; the two scroll coordinates can be used directly
+        lda temp_x
+        sta irq_table_scroll_x, x
+        lda temp_y
+        sta irq_table_scroll_y, x
+
+        ; the low nametable byte utilizes our LUT
+        ldx temp_x
+        lda nametable_low_x_table, x
+        ldx temp_y
+        ora nametable_low_y_table, x
+        ldx irq_generation_index
+        sta irq_table_nametable_low, x
+
+        ; ppumask: for debugging, make it blue!
+        lda #($1E | TINT_B)
+        sta irq_table_ppumask, x
+
+        ; finally the scanline count
+        lda (fx_scanline_table_ptr), y
+        ; if there is an initial pixel offset, subtract it here
+        sec
+        sbc initial_pixel_offset
+        sta irq_table_scanlines, x
+        
+        ; add this to temp_y for the next section
+        clc
+        adc temp_y
+        sta temp_y
+
+        ; now clear the pixel offset (if any) so that it does not apply
+        ; to any entries beyond the first
+        lda #0
+        sta initial_pixel_offset
+        
+        ; accumulate this against our running total
+        lda irq_table_scanlines, x
+        clc
+        adc pixels_generated
+        sta pixels_generated
+        ; are we through generating pixels? If so, cleanup is in order
+        cmp pixels_to_generate
+        bcs cleanup
+
+        ; advance to the next irq table entry:
+        inc irq_generation_index
+
+        ; advance to the next offset entry; if we reach the end of the table,
+        ; wrap around to the beginning
+        iny 
+        cpy fx_table_size
+        bne no_table_wrap
+        ldy #$0
+        no_table_wrap:
+
+        ; this entry is complete, advance to the next entry
+        jmp loop
+cleanup:
+        ; a holds our total generated pixels; we need to fix the scanline count for the last
+        ; entry so that it doesn't overrun the requested size
+        sec
+        sbc pixels_to_generate
+        ; now a holds the "extra" pixels that the current scanline encodes for
+        sta temp_offset
+        lda irq_table_scanlines, x ; still points to the last scanline entry
+        sec
+        sbc temp_offset
+        sta irq_table_scanlines, x ; should now contain the correct final value
+
+        ; incremenet the generation index here, so that any future generators called after this one start
+        ; in the right place and don't clobber our last entry
+        inc irq_generation_index
+
+        ; ... and we're done?
+        rts
+.endproc
+
+; a simple trainer tagged onto the end of the generated table, to display the bottom of the static
+; nametable, below the effect area
+.proc generate_final_entry
+        ldx irq_generation_index
+
+        lda base_nametable
+        and #$01
+        asl
+        asl
+        sta irq_table_nametable_high, x
+
+        lda base_x
+        sta irq_table_scroll_x, x
+        lda base_y
+        clc
+        adc #176
+        sta irq_table_scroll_y, x
+
+        ; reordered a bit, to reuse scroll_y which is already in a
+        tax
+        lda nametable_low_y_table, x
+        ldx base_x
+        ora nametable_low_x_table, x
+        ldx irq_generation_index
+        sta irq_table_nametable_low, x
+
+        ; ppumask: for debugging, make it normal
+        lda #$1E
+        sta irq_table_ppumask, x
+
+        ; the final entry specifies $FF scanlines, which lasts until NMI
+        lda #$ff
+        sta irq_table_scanlines, x
+
+        ; for consistency, increment this anyway; after any generator, we want it to
+        ; *always* point to the next valid entry. (It can also be interpreted as the
+        ; "size" of the table for debugging purposes)
+        inc irq_generation_index
+
+        rts
+.endproc
+
+.export generate_x_distortion, generate_final_entry
+
+; =========== LUT data for various generator functions =========== ;
+
+; because apparently there is no way to give ca65 a signed byte normally
+.macro sbyte value
+        .byte (value & $FF)
+.endmacro
+
+; TODO: I think this might be broken, since it still specifies deltas?
+SIMPLE_SINE_LENGTH = 16
+; note: these distortion patterns all precompute the change between
+; the previous scanline and the current one.
+simple_sine_pattern:
+        sbyte 5
+        sbyte 3
+        sbyte 3
+        sbyte 1
+        sbyte -1
+        sbyte -3
+        sbyte -3
+        sbyte -5
+        sbyte -5
+        sbyte -3
+        sbyte -3
+        sbyte -1
+        sbyte 1
+        sbyte 3
+        sbyte 3
+        sbyte 5
+
+simple_sine_scanlines:
+        .byte 4
+        .byte 4
+        .byte 4
+        .byte 4
+        .byte 4
+        .byte 4
+        .byte 4
+        .byte 4
+        .byte 4
+        .byte 4
+        .byte 4
+        .byte 4
+        .byte 4
+        .byte 4
+        .byte 4
+        .byte 4
+
+
+VERY_CURVED_SINE_LENGTH = 20
+
+very_curved_sine_pattern:
+        sbyte 0
+        sbyte 4
+        sbyte 5
+        sbyte 6
+        sbyte 7
+        sbyte 8
+        sbyte 7
+        sbyte 6
+        sbyte 5
+        sbyte 4
+        sbyte 0
+        sbyte -4
+        sbyte -5
+        sbyte -6
+        sbyte -7
+        sbyte -8
+        sbyte -7
+        sbyte -6
+        sbyte -5
+        sbyte -4
+
+very_curved_sine_scanlines:
+        .byte 1
+        .byte 1
+        .byte 2
+        .byte 2
+        .byte 4
+        .byte 13
+        .byte 4
+        .byte 2
+        .byte 2
+        .byte 1
+        .byte 1
+        .byte 1
+        .byte 2
+        .byte 2
+        .byte 4
+        .byte 13
+        .byte 4
+        .byte 2
+        .byte 2
+        .byte 1
+
+interleaved_sine_pattern:
+        sbyte 0
+        sbyte -0
+        sbyte 6
+        sbyte -6
+        sbyte 11
+        sbyte -11
+        sbyte 15
+        sbyte -15
+        sbyte 16
+        sbyte -16
+        sbyte 15
+        sbyte -15
+        sbyte 11
+        sbyte -11
+        sbyte 6
+        sbyte -6
+        sbyte 0
+        sbyte 0
+        sbyte -6
+        sbyte 6
+        sbyte -11
+        sbyte 11
+        sbyte -15
+        sbyte 15
+        sbyte -16
+        sbyte 16
+        sbyte -15
+        sbyte 15
+        sbyte -11
+        sbyte 11
+        sbyte -6
+        sbyte 6
+
+interleaved_sine_scanlines:
+        .byte 2
+        .byte 2
+        .byte 2
+        .byte 2
+        .byte 2
+        .byte 2
+        .byte 2
+        .byte 2
+        .byte 2
+        .byte 2
+        .byte 2
+        .byte 2
+        .byte 2
+        .byte 2
+        .byte 2
+        .byte 2
+        .byte 2
+        .byte 2
+        .byte 2
+        .byte 2
+        .byte 2
+        .byte 2
+        .byte 2
+        .byte 2
+        .byte 2
+        .byte 2
+        .byte 2
+        .byte 2
+        .byte 2
+        .byte 2
+        .byte 2
+        .byte 2
+
+.export interleaved_sine_pattern, interleaved_sine_scanlines
--- src/mmc3_irq_table/prg/main.s
+++ src/mmc3_irq_table/prg/main.s
@@ -1,5 +1,6 @@
         .setcpu "6502"
 
+        .include "generators.inc"
         .include "irq.inc"
         .include "nes.inc"
         .include "mmc3.inc"
@@ -12,12 +13,13 @@
 
 ptr: .word $0000
 
+        .segment "RAM"
+nmi_counter: .byte $00
+fx_offset: .byte $00
+
 camera_nametable: .byte $00
 camera_x: .byte $00
 camera_y: .byte $00
-nmi_counter: .byte $00
-
-fx_offset: .byte $00
 
         .segment "PRGLAST_E000"
         .export start, nmi
@@ -55,205 +57,6 @@ loop:
         rts
 .endproc
 
-; for static tables and code bits
-.macro nametable_low scroll_x, scroll_y
-        .byte ((scroll_x & %11111000) >> 3) | ((scroll_y & %00111000) << 2)
-.endmacro
-
-; for runtime generation without expensive bitfield manipulation, just read
-; from both tables and OR together
-nametable_low_x_table:
-        .repeat 256, scroll_x
-        .byte ((scroll_x & %11111000) >> 3)
-        .endrep
-
-nametable_low_y_table:
-        .repeat 256, scroll_y
-        .byte ((scroll_y & %00111000) << 2)
-        .endrep
-
-; a quick static test table: shift every 8th scanline to the left by 10
-; (only four times, we don't need a lot of these)
-static_test_irq_table_scanlines: 
-        .byte $01, $01, $01, $01, $01, $01, $01, $01, $01, $FF
-static_test_irq_table_nametable_high: 
-        .byte $04, $04, $04, $04, $04, $04, $04, $04, $04, $04 ; nametable (x high)
-static_test_irq_table_scroll_y: 
-        .byte $60, $64, $68, $6C, $70, $74, $78, $7C, $80, $84 ; y increases match scanline counts
-static_test_irq_table_scroll_x: 
-        .byte $FE, $FC, $FA, $F8, $F6, $F4, $F2, $F0, $EE, $EC ; x scrolls by 10px to the right on each IRQ
-static_test_irq_table_nametable_low: 
-        nametable_low $FE, $60
-        nametable_low $FC, $64
-        nametable_low $FA, $68
-        nametable_low $F8, $6C
-        nametable_low $F6, $70
-        nametable_low $F4, $74
-        nametable_low $F2, $78
-        nametable_low $F0, $7C
-        nametable_low $EE, $80
-        nametable_low $EC, $84
-
-.macro sbyte value
-        .byte (value & $FF)
-.endmacro
-
-SIMPLE_SINE_LENGTH = 16
-; note: these distortion patterns all precompute the change between
-; the previous scanline and the current one.
-simple_sine_pattern:
-        sbyte 5
-        sbyte 3
-        sbyte 3
-        sbyte 1
-        sbyte -1
-        sbyte -3
-        sbyte -3
-        sbyte -5
-        sbyte -5
-        sbyte -3
-        sbyte -3
-        sbyte -1
-        sbyte 1
-        sbyte 3
-        sbyte 3
-        sbyte 5
-
-simple_sine_scanlines:
-        .byte 4
-        .byte 4
-        .byte 4
-        .byte 4
-        .byte 4
-        .byte 4
-        .byte 4
-        .byte 4
-        .byte 4
-        .byte 4
-        .byte 4
-        .byte 4
-        .byte 4
-        .byte 4
-        .byte 4
-        .byte 4
-
-
-VERY_CURVED_SINE_LENGTH = 20
-
-very_curved_sine_pattern:
-        sbyte 0
-        sbyte 4
-        sbyte 5
-        sbyte 6
-        sbyte 7
-        sbyte 8
-        sbyte 7
-        sbyte 6
-        sbyte 5
-        sbyte 4
-        sbyte 0
-        sbyte -4
-        sbyte -5
-        sbyte -6
-        sbyte -7
-        sbyte -8
-        sbyte -7
-        sbyte -6
-        sbyte -5
-        sbyte -4
-
-very_curved_sine_scanlines:
-        .byte 1
-        .byte 1
-        .byte 2
-        .byte 2
-        .byte 4
-        .byte 13
-        .byte 4
-        .byte 2
-        .byte 2
-        .byte 1
-        .byte 1
-        .byte 1
-        .byte 2
-        .byte 2
-        .byte 4
-        .byte 13
-        .byte 4
-        .byte 2
-        .byte 2
-        .byte 1
-
-INTERLEAVED_SINE_LENGTH = 32
-
-interleaved_sine_pattern:
-        sbyte 0
-        sbyte -0
-        sbyte 6
-        sbyte -6
-        sbyte 11
-        sbyte -11
-        sbyte 15
-        sbyte -15
-        sbyte 16
-        sbyte -16
-        sbyte 15
-        sbyte -15
-        sbyte 11
-        sbyte -11
-        sbyte 6
-        sbyte -6
-        sbyte 0
-        sbyte 0
-        sbyte -6
-        sbyte 6
-        sbyte -11
-        sbyte 11
-        sbyte -15
-        sbyte 15
-        sbyte -16
-        sbyte 16
-        sbyte -15
-        sbyte 15
-        sbyte -11
-        sbyte 11
-        sbyte -6
-        sbyte 6
-
-interleaved_sine_scanlines:
-        .byte 2
-        .byte 2
-        .byte 2
-        .byte 2
-        .byte 2
-        .byte 2
-        .byte 2
-        .byte 2
-        .byte 2
-        .byte 2
-        .byte 2
-        .byte 2
-        .byte 2
-        .byte 2
-        .byte 2
-        .byte 2
-        .byte 2
-        .byte 2
-        .byte 2
-        .byte 2
-        .byte 2
-        .byte 2
-        .byte 2
-        .byte 2
-        .byte 2
-        .byte 2
-        .byte 2
-        .byte 2
-        .byte 2
-        .byte 2
-        .byte 2
-        .byte 2
-
 ; we don't really "clear" this so much as configure every entry
 ; to take more scanlines than there are in a single frame
 ; (note that irqs should be disabled during NMI if we go with this
@@ -277,193 +80,8 @@ loop:
 .endproc
 
 ; lots of this is hard-coded right now, deal
-        .segment "RAM"
-temp_x: .word $0000
-temp_y: .byte $00
-temp_offset: .byte $00
-irq_generation_index: .byte $00
-pixels_to_generate: .byte $00
-pixels_generated: .byte $00
-initial_pixel_offset: .byte $00
-
-        .zeropage
-fx_pattern_table_ptr: .word $0000
-fx_scanline_table_ptr: .word $0000
-fx_table_size: .byte $00
-
 
         .segment "PRGLAST_E000"
-.proc generate_x_distortion
-        lda #$0
-        sta pixels_generated
-        ldy #$0
-
-        ; to apply the pixel offset, skip over any initial entries that are
-        ; smaller than the offset
-        lda initial_pixel_offset
-pixel_offset_loop:
-        sec
-        sbc (fx_scanline_table_ptr), y
-        bcc done_skipping_entries
-        sta initial_pixel_offset
-        iny
-        cpy fx_table_size
-        bne no_wraparound
-        ldy #$0
-no_wraparound:
-        jmp pixel_offset_loop
-done_skipping_entries:
-        ; initial_pixel_offset has been reduced to the remainder, the number of pixels
-        ; we should skip in the *current* entry, which y points to
-
-        ; for each entry in the table (16 entries):
-        ; - compute the new scroll coordinates and nametable bit
-        ; - use these values to generate one entry in the IRQ table
-        ; note: later we'll want to be able to specify the number of entries to generate,
-        ; and wrap the table around its end with a mask
-
-loop:
-        ; reset the temp X coordinate
-        lda camera_nametable
-        sta temp_x+1
-        lda camera_x
-        sta temp_x
-
-        ; calculate the new x offset
-        lda (fx_pattern_table_ptr), y
-        sta temp_offset
-        sadd16 temp_x, temp_offset
-
-        ; generate a new entry in the table
-        ldx irq_generation_index
-
-        ; mask the low bit of the nametable, and shift it into position
-        lda temp_x+1
-        and #$01
-        asl
-        asl
-        sta irq_table_nametable_high, x
-
-        ; the two scroll coordinates can be used directly
-        lda temp_x
-        sta irq_table_scroll_x, x
-        lda temp_y
-        sta irq_table_scroll_y, x
-
-        ; the low nametable byte utilizes our LUT
-        ldx temp_x
-        lda nametable_low_x_table, x
-        ldx temp_y
-        ora nametable_low_y_table, x
-        ldx irq_generation_index
-        sta irq_table_nametable_low, x
-
-        ; ppumask: for debugging, make it blue!
-        lda #($1E | TINT_B)
-        sta irq_table_ppumask, x
-
-        ; finally the scanline count
-        lda (fx_scanline_table_ptr), y
-        ; if there is an initial pixel offset, subtract it here
-        sec
-        sbc initial_pixel_offset
-        sta irq_table_scanlines, x
-        
-        ; add this to temp_y for the next section
-        clc
-        adc temp_y
-        sta temp_y
-
-        ; now clear the pixel offset (if any) so that it does not apply
-        ; to any entries beyond the first
-        lda #0
-        sta initial_pixel_offset
-        
-        ; accumulate this against our running total
-        lda irq_table_scanlines, x
-        clc
-        adc pixels_generated
-        sta pixels_generated
-        ; are we through generating pixels? If so, cleanup is in order
-        cmp pixels_to_generate
-        bcs cleanup
-
-        ; advance to the next irq table entry:
-        inc irq_generation_index
-
-        ; advance to the next offset entry; if we reach the end of the table,
-        ; wrap around to the beginning
-        iny 
-        cpy fx_table_size
-        bne no_table_wrap
-        ldy #$0
-        no_table_wrap:
-
-        ; this entry is complete, advance to the next entry
-        jmp loop
-cleanup:
-        ; a holds our total generated pixels; we need to fix the scanline count for the last
-        ; entry so that it doesn't overrun the requested size
-        sec
-        sbc pixels_to_generate
-        ; now a holds the "extra" pixels that the current scanline encodes for
-        sta temp_offset
-        lda irq_table_scanlines, x ; still points to the last scanline entry
-        sec
-        sbc temp_offset
-        sta irq_table_scanlines, x ; should now contain the correct final value
-
-        ; incremenet the generation index here, so that any future generators called after this one start
-        ; in the right place and don't clobber our last entry
-        inc irq_generation_index
-
-        ; ... and we're done?
-        rts
-.endproc
-
-; a simple trainer tagged onto the end of the generated table, to display the bottom of the static
-; nametable, below the effect area
-.proc generate_final_entry
-        ldx irq_generation_index
-        
-
-        lda camera_nametable
-        and #$01
-        asl
-        asl
-        sta irq_table_nametable_high, x
-
-        lda camera_x
-        sta irq_table_scroll_x, x
-        lda camera_y
-        clc
-        adc #(32 + 176)
-        sta irq_table_scroll_y, x
-
-        ; reordered a bit, to reuse scroll_y which is already in a
-        tax
-        lda nametable_low_y_table, x
-        ldx camera_x
-        ora nametable_low_x_table, x
-        ldx irq_generation_index
-        sta irq_table_nametable_low, x
-
-        ; ppumask: for debugging, make it normal
-        lda #$1E
-        sta irq_table_ppumask, x
-
-        ; the final entry specifies $FF scanlines, which lasts until NMI
-        lda #$ff
-        sta irq_table_scanlines, x
-
-        ; for consistency, increment this anyway; after any generator, we want it to
-        ; *always* point to the next valid entry. (It can also be interpreted as the
-        ; "size" of the table for debugging purposes)
-        inc irq_generation_index
-
-        rts
-.endproc
-
 .proc wait_for_nmi
         lda nmi_counter
 loop:
@@ -556,19 +174,15 @@ no_wrap:
 
         ; initialize scroll registers
         lda camera_nametable
-        sta temp_x+1
+        sta base_nametable
         lda camera_x
-        sta temp_x
+        sta base_x
         lda camera_y
         ; apply initial offset to get us to the starting point of the distortion effect
         ; note: later we'll want to vary the initial offset
         clc
         adc #32
-        sta temp_y
-
-        ;st16 fx_pattern_table_ptr, very_curved_sine_pattern
-        ;st16 fx_scanline_table_ptr, very_curved_sine_scanlines
-        ;lda #(VERY_CURVED_SINE_LENGTH)
+        sta base_y
 
         st16 fx_pattern_table_ptr, interleaved_sine_pattern
         st16 fx_scanline_table_ptr, interleaved_sine_scanlines
