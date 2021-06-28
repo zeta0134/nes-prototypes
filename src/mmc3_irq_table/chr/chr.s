affb91bf679f53101160c6e13305798a258e88af
Nicholas Flynt <thegamefreak0134@gmail.com>
2021-06-28 13:02:59 -0400

    Add an inverted copy of the nes tileset, for CHR swap testing

--- src/mmc3_irq_table/chr/chr.s
+++ src/mmc3_irq_table/chr/chr.s
@@ -1,7 +1,9 @@
         .setcpu "6502"
 
-.scope CHR0
         .segment "CHR0"
         .align $800
         .incbin "nes-tiles.chr"
-.endscope
+
+        .segment "CHR1"
+        .align $800
+        .incbin "nes-tiles-inverted.chr"
