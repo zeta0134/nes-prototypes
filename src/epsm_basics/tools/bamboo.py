5afe2058125717ee08f10a188d43192d89466945
zeta0134 <zeta0134@users.noreply.github.com>
2021-10-23 14:28:42 -0400

    Read in the basics of FM and SSG instruments
    Note that for the moment, I am choosing to ignore most FM sequences, largely because I do not believe the 6502 has the processing power to deal with them in a useful way. I haven't run into any modules in the wild that use them, seems to be a bit of an advanced technique.

--- src/epsm_basics/tools/bamboo.py
+++ src/epsm_basics/tools/bamboo.py
@@ -41,6 +41,34 @@ class Mixer:
             return "Mixer(%s, fm: %s, ssg: %s)" % (self.mixer_type, self.custom_fm, self.custom_ssg)
         return "Mixer(%s)" % (self.mixer_type)
 
+class InstrumentType(Enum):
+    FM = 0x00
+    SSG = 0x01
+    ADPCM = 0x02
+    DRUMKIT = 0x03
+
+class FmInstrument:
+    def __init__(self, name="", envelope_index=0, lfo_index=None):
+        self.name = name
+        self.envelope_index = envelope_index
+        self.lfo_index = lfo_index
+
+    def __repr__(self):
+        return "FmInstrument('%s')" % (self.name)
+
+class SsgInstrument:
+    def __init__(self, name="", waveform_sequence_index=None, tone_noise_sequence_index=None,
+        envelope_sequence_index=None, arpeggio_sequence_index=None, pitch_sequence_index=None):
+        self.name = name
+        self.waveform_sequence_index = waveform_sequence_index
+        self.tone_noise_sequence_index = tone_noise_sequence_index
+        self.envelope_sequence_index = envelope_sequence_index
+        self.arpeggio_sequence_index = arpeggio_sequence_index
+        self.pitch_sequence_index = waveform_sequence_index
+
+    def __repr__(self):
+        return "SsgInstrument('%s')" % (self.name)
+
 class BambooTrackerModule:
     def __init__(self, title="", author="", copyright="", comment="", tick_frequency=60, 
             first_highlight_step=4, second_highlight_step=4, mixer=None):
@@ -144,6 +172,70 @@ def _read_module_section(section_contents):
             second_highlight_step=second_highlight_step, mixer=mixer)
     return module
 
+def sequence_index(flags_byte):
+    if flags_byte & 0x80 != 0:
+        return flags_byte & 0x7F
+    return None
+
+def _read_fm_instrument(instrument_name, instrument_contents):
+    (envelope_number, instrument_contents) = _consume_uint8_from(instrument_contents)
+    (lfo_byte, instrument_contents) = _consume_uint8_from(instrument_contents)
+    lfo_index = sequence_index(lfo_byte)
+    return FmInstrument(instrument_name, envelope_number, lfo_index)
+
+def _read_ssg_instrument(instrument_name, instrument_contents):
+    (waveform_byte, instrument_contents) = _consume_uint8_from(instrument_contents)
+    (tone_noise_byte, instrument_contents) = _consume_uint8_from(instrument_contents)
+    (envelope_byte, instrument_contents) = _consume_uint8_from(instrument_contents)
+    (arpeggio_byte, instrument_contents) = _consume_uint8_from(instrument_contents)
+    (pitch_byte, instrument_contents) = _consume_uint8_from(instrument_contents)
+    waveform_sequence_index = sequence_index(waveform_byte)
+    tone_noise_sequence_index = sequence_index(tone_noise_byte)
+    envelope_sequence_index = sequence_index(envelope_byte)
+    arpeggio_sequence_index = sequence_index(arpeggio_byte)
+    pitch_sequence_index = sequence_index(pitch_byte)
+    return SsgInstrument(instrument_name, waveform_sequence_index, tone_noise_sequence_index, 
+        envelope_sequence_index, arpeggio_sequence_index, pitch_sequence_index)
+
+def _read_adpcm_instrument(instrument_name, instrument_contents):
+    print("ADPCM instrument: ", instrument_name)
+    print("Unimplemented!")
+    return None
+
+def _read_drumkit_instrument(instrument_name, instrument_contents):
+    print("Drumkit instrument: ", instrument_name)
+    print("Unimplemented!")
+    return None
+
+def _read_instrument(instrument_contents):
+    (name, instrument_contents) = _consume_pascal_string_from(instrument_contents)
+    (type_raw, instrument_contents) = _consume_uint8_from(instrument_contents)
+    instrument_type = InstrumentType(type_raw)
+    if instrument_type == InstrumentType.FM:
+        return _read_fm_instrument(name, instrument_contents)
+    if instrument_type == InstrumentType.SSG:
+        return _read_ssg_instrument(name, instrument_contents)
+    if instrument_type == InstrumentType.ADPCM:
+        return _read_adpcm_instrument(name, instrument_contents)
+    if instrument_type == InstrumentType.DRUMKIT:
+        return _read_drumkit_instrument(name, instrument_contents)
+    return None
+
+def _read_instrument_section(section_contents):
+    (instrument_count, section_contents) = _consume_uint8_from(section_contents)
+    instruments = {}
+    for i in range(0, instrument_count):
+        # common header
+        (instrument_index, section_contents) = _consume_uint8_from(section_contents)
+        (end_of_instrument_offset, section_contents) = _consume_uint32_from(section_contents)
+        instrument_length = end_of_instrument_offset - 4
+        instrument_contents = section_contents[0:instrument_length]
+        section_contents = section_contents[instrument_length:len(section_contents)]
+        instruments[instrument_index] = _read_instrument(instrument_contents)
+    for index in instruments:
+        print("%02x: %s" % (index, instruments[index]))
+    return instruments
+
 test_filename = "ponicanyon.btm"
 with open(test_filename, 'rb') as module_file:
     raw_data = module_file.read()
@@ -162,5 +254,7 @@ with open(test_filename, 'rb') as module_file:
     print(module)
     print(module.mixer)
 
+    if sections["INSTRMNT"]:
+        module.instruments = _read_instrument_section(sections["INSTRMNT"])
 
 
