1e1e5157b2a2358d81c15bdc1cc3ed89a1567127
Zeta <zeta0134@reploid.cafe>
2023-10-14 04:21:49 -0400

    Write up lots of commands and get a basic memory dumper working

--- src/in_memory_stub/prg/main.s
+++ src/in_memory_stub/prg/main.s
@@ -21,6 +21,9 @@ tile_base: .res 1
 oam_cycle_counter: .res 1
 debug_sprite_palette: .res 1
 current_output_sprite: .res 1
+connection_established: .res 1
+
+crc16: .res 2
 
         .segment "RAMSTUB"
         .export start
@@ -186,7 +189,7 @@ no_change:
 ; intentionally slow down the protocol at various points, so the arduino
 ; can keep up
 .proc burn_some_time
-        .repeat 16
+        .repeat 8
         nop
         .endrepeat
         rts
@@ -195,8 +198,8 @@ no_change:
 ; result in A, bytes read in X
 ; (this function only reads up to 1 byte, but X may range from 0-15)
 ; If X=0, A=undefined
-.proc read_byte
-ScratchByte := R0
+.proc raw_read_byte
+ScratchByte := R15
         ; first, initialize the port and make sure it's in a clean state
         lda $4016
 
@@ -234,10 +237,39 @@ done:
         rts
 .endproc
 
+; like read_byte, but patiently waits for up to Y attempts for there to
+; be data in the buffer. useful if the remote end is not very fast
+; upon exit:
+; X = buffer length (nonzero for success)
+; A = data (undefined if X=0)
+; Y = clobbered
+.proc read_byte_with_retries
+retry_loop:
+        jsr raw_read_byte
+        cpx #0
+        bne done
+        cpy #0
+        beq done
+        dey
+        jmp retry_loop
+done:
+        rts
+.endproc
+
+; default single byte reading routine. Tries up to 10 times before
+; giving up, which is typically enough to shake the cobwebs out of the connection
+; TODO: if we fail to read a byte when one was expected, should we terminate the connection
+; and display an error?
+.proc read_byte
+        ldy #10
+        jsr read_byte_with_retries
+        rts        
+.endproc
+
 ; byte to write goes in A
 .proc write_byte
-ScratchByte := R0
-Counter := R1
+ScratchByte := R14
+Counter := R15
         sta ScratchByte
 
         ; first, initialize the port and make sure it's in a clean state
@@ -311,28 +343,226 @@ no_action:
         rts
 .endproc
 
-.proc debug_echoply
-TempStash := R15
+TOTAL_COMMANDS = 7
+
+.proc process_commands
+CommandByte := R0
+CommandPtr := R1
+        jsr read_byte
+        cpx #0
+        beq no_command
+        sta CommandByte
+        
+        lda connection_established
+        bne all_commands_allowed
+        ; initially, only command $00 is allowed, which is the signature command
+        ; once a connection is established, we allow the full set
+        lda CommandByte
+        bne no_command
+all_commands_allowed:
+        ; make sure this command is in range
+        lda CommandByte
+        cmp #TOTAL_COMMANDS
+        bcs no_command
+        ; convert this into a command ptr
+        asl
+        tax
+        lda command_table+0, x
+        sta CommandPtr
+        lda command_table+1, x
+        sta CommandPtr+1
+        jmp (CommandPtr)
+
+no_command:
+        rts
+.endproc
+
+command_table:
+        .addr cmd_signature
+        .addr cmd_read_byte_cpu
+        .addr cmd_write_byte_cpu
+        .addr cmd_call_subroutine
+        .addr cmd_close_connection
+        .addr cmd_checksum_region
+        .addr cmd_read_page
+
+.proc cmd_signature
+        ; expects the two data bytes $01 and $34. Upon success, will reply with
+        ; its own signature: $42, $01, $34, and enable other commands
+
+         ; if either byte read fails, ignore this command and exit
         jsr read_byte
         cpx #0
         beq done
-        ; A contains the byte we just read; multiply it by 2,
-        ; then send it back out the pipe
-        sta TempStash
-        asl
-        ; debug: send a fixed value instead!
-        lda #$13
+        cmp #$01
+        bne done
+
+        jsr read_byte
+        cpx #0
+        beq done
+        cmp #$34
+        bne done
+
+        ; signature verified! firstly, mark the connection as open, which permits
+        ; other commands
+        lda #1
+        sta connection_established
+
+        ; now we can respond with our own signature
+        lda #$42
         jsr write_byte
-        ; while we're in here, let's also write that byte to the
-        ; output buffer, so we can see what we're doing onscreen
-        lda TempStash
-        ldx current_output_sprite
-        jsr debug_display_sprite
-        inc current_output_sprite
-        lda current_output_sprite
-        and #$0F
-        sta current_output_sprite
+        lda #$01
+        jsr write_byte
+        lda #$34
+        jsr write_byte
+
 done:
+        rts
+.endproc
+
+.proc cmd_read_byte_cpu
+CpuAddr := R0
+        ; the next two bytes will be the address to read, in little-endian
+        jsr read_byte
+        sta CpuAddr+0
+        jsr read_byte
+        sta CpuAddr+1
+        ; perform the read, then transmit the result over the wire
+        ldy #0
+        lda (CpuAddr), y
+        jsr write_byte
+
+        rts
+.endproc
+
+.proc cmd_write_byte_cpu
+CpuAddr := R0
+        ; the next two bytes will be the address to read, in little-endian
+        jsr read_byte
+        sta CpuAddr+0
+        jsr read_byte
+        sta CpuAddr+1
+        ; then the byte we want to write
+        jsr read_byte
+        ; perform the write, then we're done (no reply for this command)
+        ldy #0
+        sta (CpuAddr), y
+
+        rts
+.endproc
+
+.proc cmd_call_subroutine
+SubroutineAddr := R0
+        ; the subroutine address is the only argument, in little endian
+        ; (no parameter passing, use temporary zeropage to establish that if you need it)
+        jsr read_byte
+        sta SubroutineAddr+0
+        jsr read_byte
+        sta SubroutineAddr+1
+        ; I sure hope that was a valid subroutine address, because we're going there now!
+        jmp (SubroutineAddr)
+        ; (when the subroutine returns, it will exit the command dispatcher, which is fine)
+.endproc
+
+.proc cmd_close_connection
+        ; "Goodbye!"
+        lda #0
+        sta connection_established
+        rts
+.endproc
+
+; computes a 16bit checksum of the provided memory region and returns that
+; checksum over the wire. Slow, but useful for verifying large transfers
+
+.proc cmd_checksum_region
+StartAddr := R0
+Length := R2
+        jsr read_byte
+        sta StartAddr+0
+        jsr read_byte
+        sta StartAddr+1
+
+        jsr read_byte
+        sta Length+0
+        jsr read_byte
+        sta Length+1
+
+        lda #$FF
+        sta crc16+0
+        sta crc16+1
+
+        ; sanity check: is length 0? if so, exit now
+        lda Length+0
+        ora Length+1
+        beq transmit_result
+
+loop:
+        ldy #0
+        lda (StartAddr), y
+        jsr crc16_consume_one_byte ; clobbers everything, including Y
+        inc16 StartAddr
+        dec16 Length
+        lda Length
+        ora Length+1
+        bne loop
+
+transmit_result:
+        lda crc16+0
+        jsr write_byte
+        lda crc16+1
+        jsr write_byte
+
+        rts
+.endproc
+
+; adapted from http://www.6502.org/source/integers/crc-more.html
+; byte in A, clobbers everything
+.proc crc16_consume_one_byte
+CRCLO := crc16+0
+CRCHI := crc16+1
+        eor CRCHI       ; A contained the data
+        sta CRCHI       ; XOR it into high byte
+        lsr             ; right shift A 4 bits
+        lsr             ; to make top of x^12 term
+        lsr             ; ($1...)
+        lsr
+        tax             ; save it
+        asl             ; then make top of x^5 term
+        eor CRCLO       ; and XOR that with low byte
+        sta CRCLO       ; and save
+        txa             ; restore partial term
+        eor CRCHI       ; and update high byte
+        sta CRCHI       ; and save
+        asl             ; left shift three
+        asl             ; the rest of the terms
+        asl             ; have feedback from x^12
+        tax             ; save bottom of x^12
+        asl             ; left shift two more
+        asl             ; watch the carry flag
+        eor CRCHI       ; bottom of x^5 ($..2.)
+        tay             ; save high byte
+        txa             ; fetch temp value
+        rol             ; bottom of x^12, middle of x^5!
+        eor CRCLO       ; finally update low byte
+        sta CRCHI       ; then swap high and low bytes
+        sty CRCLO
+        rts
+.endproc
+
+.proc cmd_read_page
+PageAddr := R0
+        lda #0
+        sta PageAddr+0
+        jsr read_byte
+        sta PageAddr+1
+
+        ldy #0
+loop:
+        lda (PageAddr), y
+        jsr write_byte
+        iny
+        bne loop
+
         rts
 .endproc
 
@@ -373,6 +603,7 @@ done:
         lda #0
         sta tile_base
         sta debug_sprite_palette
+        sta connection_established
         jsr draw_demo_oam_strip
         lda #60
         sta oam_cycle_counter
@@ -381,8 +612,7 @@ done:
 gameloop:
         ; any game loop logic would go here
         jsr debug_heartbeat
-        jsr debug_echoply
-
+        jsr process_commands
 
         ; now we wait for, and then do vblank things
         jsr wait_for_vblank_ish
--- src/in_memory_stub/tools/serial_test.py
+++ src/in_memory_stub/tools/serial_test.py
@@ -2,26 +2,118 @@
 
 import serial
 import time
+import crcmod.predefined
+
+crc16func = crcmod.predefined.mkCrcFun('crc-ccitt-false')
 
 port = "/dev/ttyACM1"
-
-famicom = serial.Serial(port, 9600, timeout=10)
+famicom = serial.Serial(port, 115200, timeout=10)
 # arduino resets when establishing a new serial connection, so we need a brief
 # sleep here to give it time to get its ducks in a row
 time.sleep(2)
 print("Serial connection established")
 
-# write a single byte to the famicom
-data_byte = 0x42
-famicom.write([data_byte])
-print(f"Sent 0x{data_byte:02X}...")
-print("Attempting to read reply...")
+CMD_OPEN_CONNECTION  = 0x00
+CMD_READ_BYTE_CPU    = 0x01
+CMD_WRITE_BYTE_CPU   = 0x02
+CMD_CALL_SUBROUTINE  = 0x03
+CMD_CLOSE_CONNECTION = 0x04
+CMD_CHECKSUM_REGION  = 0x05
+CMD_READ_PAGE_CPU    = 0x06
 
-# now read a byte back, and display it
-result_bytes = famicom.read(size=1)
-if len(result_bytes) > 0:
-    result_byte = int(result_bytes[0])
-    print(f"Got 0x{result_byte:02X} from famicom!")
+def write_byte_raw(data):
+    famicom.write([data])
 
+def read_byte_raw():
+    reply = famicom.read(size=1)
+    if len(reply) == 0:
+        return (0, False)
+    return (int(reply[0]), True)
 
+def cmd_open_connection():
+    # send our signature command over the wire
+    write_byte_raw(CMD_OPEN_CONNECTION)
+    write_byte_raw(0x01)
+    write_byte_raw(0x34)
+
+    # read back the reply, and verify the remote signature
+    expected_reply = [0x42, 0x01, 0x34]
+    for expected_byte in expected_reply:
+        (actual_byte, success) = read_byte_raw()
+        if not success or actual_byte != expected_byte:
+            return False
+    return True
+
+def cmd_read_byte_cpu(address):
+    write_byte_raw(CMD_READ_BYTE_CPU)
+    write_byte_raw(address & 0x00FF)
+    write_byte_raw((address & 0xFF00) >> 8)
+    (data_byte, success) = read_byte_raw()
+    return data_byte
+
+def cmd_write_byte_cpu(address, data):
+    write_byte_raw(CMD_WRITE_BYTE_CPU)
+    write_byte_raw(address & 0x00FF)
+    write_byte_raw((address & 0xFF00) >> 8)
+    write_byte_raw(data)
+
+def cmd_call_subroutine(address):
+    write_byte_raw(CMD_CALL_SUBROUTINE)
+    write_byte_raw(address & 0x00FF)
+    write_byte_raw((address & 0xFF00) >> 8)
+
+def cmd_close_connection():
+    write_byte_raw(CMD_CLOSE_CONNECTION)
+
+def cmd_checksum_region(address, length):
+    write_byte_raw(CMD_CHECKSUM_REGION)
+    write_byte_raw(address & 0x00FF)
+    write_byte_raw((address & 0xFF00) >> 8)
+    write_byte_raw(length & 0x00FF)
+    write_byte_raw((length & 0xFF00) >> 8)
+    (crc16low, success) = read_byte_raw()
+    (crc16high, success) = read_byte_raw()
+    crc16 = ((crc16high << 8) | crc16low)
+    return crc16
+
+def cmd_read_page_cpu(high_addr):
+    write_byte_raw(CMD_READ_PAGE_CPU)
+    write_byte_raw(high_addr)
+
+    page_bytes = []
+    for i in range(0, 256):
+        (data_byte, success) = read_byte_raw()
+        page_bytes.append(data_byte)
+    return page_bytes
+
+if cmd_open_connection():
+    print("Signature verified! We're in business!")
+    print("Proceeding to dump memory region from 0xE000 - 0xFFFF")
+
+    data_dump = []
+    #for cpu_addr in range(0xE000, 0xFFFF + 1):
+    #    raw_byte = cmd_read_byte_cpu(cpu_addr)
+    #    # print(f"From address ${cpu_addr:04X} got byte ${raw_byte:02X}")
+    #    data_dump.append(raw_byte)
+    #    if cpu_addr % 0x100 == 0:
+    #        print(f"{cpu_addr:04X}...")
+    for page in range(0xE0, 0xFF + 1):
+        print(f"Reading page: ${page:02X}00...")
+        page_bytes = cmd_read_page_cpu(page)
+        data_dump = data_dump + page_bytes
+    print("Dump completed, writing to file!")
+    with open("out.dump.bin", "wb") as outfile:
+        outfile.write(bytes(data_dump))
+
+    print("Asking famicom to perform CRC16 of dumped region...")
+    expected_crc = cmd_checksum_region(0xE000, 0x2000)
+    actual_crc = crc16func(bytes(data_dump))
+
+    print(f"Reported CRC16 of dumped region: ${expected_crc:04X}")
+    print(f"Actual CRC16 of dumped region: ${actual_crc:04X}")
+
+    print("Operation succeeded, closing connection")
+    cmd_close_connection()
+else:
+    print("Failed to verify signature, cannot proceed.")
 
